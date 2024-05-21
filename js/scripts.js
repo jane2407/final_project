2054ce38d406d3995e26560a2efc0ba9b46829c1
jane2407 <e.s.aleksandrova2014@gmail.com>
2024-05-21 21:14:20 +0300

    media

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 $('.owl-carousel').owlCarousel({
     autoplay:true,
-    autoplayTimeout: 1000,
+    autoplayTimeout: 10000,
     loop:true,
-    responsive:{
-        0:{
-            items:1
+    margin:64,
+    responsive: {
+        0: {
+            items: 1
         },
-        500:{
-            items:4
-        },
-        1100:{
-            items:8
+
+        1100: {
+            items: 3
         }
     }
 });
